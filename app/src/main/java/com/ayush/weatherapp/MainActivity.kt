0fa9dd41c997f8f2d9cd2c30e91e543447ff8832
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-04 14:58:18 +0530

    made a launcher splash screen and now working on main ui of the app

--- app/src/main/java/com/ayush/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ayush/weatherapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.ayush.weatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timerSwitch() // called time switch method to change the activity
+    }
+    fun timerSwitch(){
+        val timeTask = timerTask {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        val timer = Timer()
+        timer.schedule(timeTask,3000)
     }
 }
