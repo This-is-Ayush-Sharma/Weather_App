8054d6e653be4db8c3d614044a9b0f965fd44a30
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-04 15:43:47 +0530

    done with the ui of the app.

--- app/src/main/java/com/ayush/weatherapp/MainActivity2.kt
+++ app/src/main/java/com/ayush/weatherapp/MainActivity2.kt
@@ -1,11 +1,26 @@
 package com.ayush.weatherapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var date:TextView
+    lateinit var time:TextView
+    lateinit var temp:TextView
+    lateinit var humi:TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        // find the view of the activities.
+        date = findViewById(R.id.FetchDate)
+        time = findViewById(R.id.fetchTime)
+        temp = findViewById(R.id.fetchTemprature)
+        humi = findViewById(R.id.fetchHumidity)
+
+
     }
 }
